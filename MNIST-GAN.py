de051399661e1d48963e7951b7bc4c6dbd989a3a
Nick Cho <nicholas.cho@hotmail.ca>
2022-03-08 20:46:18 -0500

    Initial Model with training code

--- MNIST-GAN.py
+++ MNIST-GAN.py
@@ -77,4 +77,71 @@ combined_model = Model(z, fake_pred)
 # Compile Combined model to adjust weights
 combined_model.compile(loss='binary_crossentropy', optimizer=Adam(0.0002, 0.5))
 
-# training the GAN
+# Training the GAN
+
+batch_size = 32
+epochs = 40000
+sample_period = 200  # data is saved every time the sample period passes
+
+# Creating Batch Labels
+ones = np.ones(batch_size)
+zeros = np.zeros(batch_size)
+
+d_losses = []  # discriminator loss
+g_losses = []  # generator loss
+
+# Creating folder to store images
+if not os.path.exists('gan_images'):
+    os.makedirs('gan_images')
+
+
+def sample_images(epoch):
+    rows, cols = 5, 5
+    # creating noise to input into the generator
+    noise = np.random.randn(rows*cols, latent_dim)
+    imgs = generator.predict(noise)
+
+    # Rescale images to 0-1
+    imgs = 0.5 * imgs + 0.5
+
+    fig, axs = plt.subplots(rows, cols)
+    idx = 0
+    for i in range(rows):
+        for j in range(cols):
+            axs[i, j].imshow(imgs[idx].reshape(H, W), cmap='gray')
+            axs[i, j].axis('off')
+            idx += 1
+    fig.savefig("gan_images/%d.png" % epoch)
+    plt.close()
+
+
+for epoch in range(epochs):
+    # Training discriminator
+    idx = np.random.randint(0, x_train.shape[0], batch_size)
+    real_imgs = x_train[idx]
+
+    noise = np.random.randn(batch_size, latent_dim)
+    fake_imgs = generator.predict(noise)
+
+    d_loss_real, d_acc_real = discriminator.train_on_batch(real_imgs, ones)
+    d_loss_fake, d_acc_fake = discriminator.train_on_batch(fake_imgs, zeros)
+    d_loss = 0.5 * (d_loss_real + d_loss_fake)
+    d_acc = 0.5 * (d_acc_real + d_acc_fake)
+
+    # Training generator
+    noise = np.random.randn(batch_size, latent_dim)
+    g_loss = combined_model.train_on_batch(noise, ones)
+
+    noise = np.random.randn(batch_size, latent_dim)
+    g_loss = combined_model.train_on_batch(noise, ones)
+
+    # Save the losses
+    d_losses.append(d_loss)
+    g_losses.append(g_loss)
+
+    if epoch % 100 == 0:
+        print(f"epoch: {epoch+1}/{epochs}, d_loss: {d_loss:.2f}, \
+      d_acc: {d_acc:.2f}, g_loss: {g_loss:.2f}")
+
+    if epoch % sample_period == 0:
+        sample_images(epoch)
